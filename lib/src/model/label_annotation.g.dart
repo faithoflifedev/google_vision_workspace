8382e456b71fa09bb0b9697c54c9b59e1393f887
4ctobias <tobias@4c.media>
2022-06-18 15:28:50 +0200

    Added support for label detection in AnnotatedResponse

---
+++ lib/src/model/label_annotation.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'label_annotation.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LabelAnnotation _$LabelAnnotationFromJson(
+        Map<String, dynamic> json) =>
+    LabelAnnotation(
+      mid: json['mid'] as String,
+        description: json['description'] as String,
+      score: (json['score'] as num).toDouble(),
+      topicality: (json['topicality'] as num).toDouble()
+    );
+
+Map<String, dynamic> _$LabelAnnotationToJson(
+        LabelAnnotation instance) =>
+    <String, dynamic>{
+      'mid': instance.mid,
+      'description': instance.description,
+      'score': instance.score,
+      'topicality': instance.topicality,
+    };
